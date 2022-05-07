72a5bcd0e79f54fa050a8f9721a2a5cade622708
sam bacha <sam@manifoldfinance.com>
2022-05-07 10:03:51 -0700

    chore(NeedURLMessage): clarify RPC endpoint usage

--- src/components/needURLMessage.js
+++ src/components/needURLMessage.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+/**
+* @const NeedURLMessage
+* Enter an RPC Endpoint URL
+*/
 const NeedURLMessage = () => {
   return (
     <div>
       <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide mb-4">
-        Please ENTER a Web3 URL in the nav bar
+        Please ENTER a RPC Endpoint URL in the nav bar
       </h2>
       <hr className="my-2" />
       <p className="text-xs mb-3">
-        We need you to drop a Web3 URL in the input field at the top before we
-        can do anything meaningful.
+        Enter an RPC Provider Endpoint URL to check for issues
+        Example:
+        `https://api.sushirelay.com/v1` 
       </p>
     </div>
   );
