2bf650fe6f8306a306967385650e5bfc8b88e5e4
sam bacha <sam@manifoldfinance.com>
2022-05-07 10:01:33 -0700

    chore(NeedLibMessage): clarify usage

--- src/components/needLibMessage.js
+++ src/components/needLibMessage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+/**
+* @const NeedLibMessage
+* Selects either Web3js or Ethers-io Library
+*/
 const NeedLibMessage = () => {
   return (
     <div className="py-2">
@@ -11,8 +15,8 @@ const NeedLibMessage = () => {
       </h2>
       <hr className="my-2" />
       <p className="text-xs mb-3">
-        Select a web3 library and this area will populate with a list of support
-        RPC methods for you to choose from.
+        Select a Web3 library and this area will populate with a list of 
+        supported RPC methods for you to choose from.
       </p>
     </div>
   );
