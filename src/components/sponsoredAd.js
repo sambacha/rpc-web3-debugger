e27afa384b7ce653d50691fd9d254e6c5268090c
sam bacha <sam@manifoldfinance.com>
2022-05-07 09:58:53 -0700

    chore(link): telegram link

--- src/components/sponsoredAd.js
+++ src/components/sponsoredAd.js
@@ -33,12 +33,12 @@ const SponsoredAd = ({ visible, setSponsorVisible }) => {
                 <div className="mt-4 flex">
                   <span className="inline-flex rounded-md shadow-sm">
                     <a
-                      href="https://manifoldfinance.com"
+                      href="https://t.me/manifoldfinance"
                       className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
                       target="_blank"
                       rel="noopener"
                     >
-                      Visit Manifold Finance
+                      Telegram Manifold Finance
                     </a>
                   </span>
                 </div>
