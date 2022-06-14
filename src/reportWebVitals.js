b1178aad66190073fac452a918535a031a95b8bc
sam bacha <sam@manifoldfinance.com>
2022-06-14 06:31:32 -0700

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -6,8 +6,25 @@ const reportWebVitals = (onPerfEntry) => {
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
+      getCLS(console.log);
+      getFID(console.log);
+      getLCP(console.log);
     });
   }
 };
+
+function sendToAnalytics(metric) {
+  // Replace with whatever serialization method you prefer.
+  // Note: JSON.stringify will likely include more data than you need.
+  const body = JSON.stringify(metric);
+
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+      fetch('/analytics', {body, method: 'POST', keepalive: true});
+}
+
+getCLS(sendToAnalytics);
+getFID(sendToAnalytics);
+getLCP(sendToAnalytics);
 
 export default reportWebVitals;
